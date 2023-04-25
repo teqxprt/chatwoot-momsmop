c31f8ff57894b581c996aa67878cdd5bd7e4b44d
Shivam Mishra <scm.mymail@gmail.com>
2023-04-25 09:47:47 +0530

    feat: sort conversation on priority (#6943)
    * feat: update seed script to include prioritt
    
    * feat: add sort_handler for conversations
    
    * test: sort on priority order
    
    ---------
    
    Co-authored-by: Muhsin Keloth <muhsinkeramam@gmail.com>

---
+++ app/javascript/shared/helpers/BaseActionCableConnector.js
@@ -0,0 +1,73 @@
+import { createConsumer } from '@rails/actioncable';
+import { BUS_EVENTS } from 'shared/constants/busEvents';
+
+const PRESENCE_INTERVAL = 20000;
+
+class BaseActionCableConnector {
+  static isDisconnected = false;
+
+  constructor(app, pubsubToken, websocketHost = '') {
+    const websocketURL = websocketHost ? `${websocketHost}/cable` : undefined;
+
+    this.consumer = createConsumer(websocketURL);
+    this.subscription = this.consumer.subscriptions.create(
+      {
+        channel: 'RoomChannel',
+        pubsub_token: pubsubToken,
+        account_id: app.$store.getters.getCurrentAccountId,
+        user_id: app.$store.getters.getCurrentUserID,
+      },
+      {
+        updatePresence() {
+          this.perform('update_presence');
+        },
+        received: this.onReceived,
+        disconnected: () => {
+          BaseActionCableConnector.isDisconnected = true;
+          this.onDisconnected();
+          // TODO: Remove this after completing the conversation list refetching
+          window.bus.$emit(BUS_EVENTS.WEBSOCKET_DISCONNECT);
+        },
+      }
+    );
+    this.app = app;
+    this.events = {};
+    this.isAValidEvent = () => true;
+    this.triggerPresenceInterval = () => {
+      setTimeout(() => {
+        this.subscription.updatePresence();
+        this.checkConnection();
+        this.triggerPresenceInterval();
+      }, PRESENCE_INTERVAL);
+    };
+    this.triggerPresenceInterval();
+  }
+
+  checkConnection() {
+    const isConnectionActive = this.consumer.connection.isOpen();
+    const isReconnected =
+      BaseActionCableConnector.isDisconnected && isConnectionActive;
+    if (isReconnected) {
+      this.onReconnect();
+      BaseActionCableConnector.isDisconnected = false;
+    }
+  }
+
+  onReconnect = () => {};
+
+  onDisconnected = () => {};
+
+  disconnect() {
+    this.consumer.disconnect();
+  }
+
+  onReceived = ({ event, data } = {}) => {
+    if (this.isAValidEvent(data)) {
+      if (this.events[event] && typeof this.events[event] === 'function') {
+        this.events[event](data);
+      }
+    }
+  };
+}
+
+export default BaseActionCableConnector;
