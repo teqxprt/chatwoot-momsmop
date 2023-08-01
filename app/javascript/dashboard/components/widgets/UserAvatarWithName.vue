bc3415b1957ac73a462fb1fc435687f5b2e69e0e
Sivin Varghese <64252451+iamsivin@users.noreply.github.com>
2023-08-01 10:39:38 +0530

    Merge branch 'develop' into CW-2128

---
+++ app/javascript/dashboard/components/widgets/UserAvatarWithName.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row--user-block">
+    <thumbnail
+      :src="user.thumbnail"
+      :size="size"
+      :username="user.name"
+      :status="user.availability_status"
+    />
+    <h6
+      class="text-block-title user-name overflow-hidden whitespace-nowrap text-ellipsis text-capitalize"
+    >
+      {{ user.name }}
+    </h6>
+  </div>
+</template>
+<script>
+import Thumbnail from 'dashboard/components/widgets/Thumbnail.vue';
+
+export default {
+  components: {
+    Thumbnail,
+  },
+  props: {
+    user: {
+      type: Object,
+      default: () => {},
+    },
+    size: {
+      type: String,
+      default: '20px',
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.row--user-block {
+  @apply items-center flex text-left;
+
+  .user-name {
+    @apply my-0 mx-2 text-slate-700 dark:text-slate-100;
+  }
+}
+</style>
---
+++ spec/models/channel/email_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require Rails.root.join 'spec/models/concerns/reauthorizable_shared.rb'
+
+RSpec.describe Channel::Email do
+  let(:channel) { create(:channel_email) }
+
+  describe 'concerns' do
+    it_behaves_like 'reauthorizable'
+
+    context 'when prompt_reauthorization!' do
+      it 'calls channel notifier mail for email' do
+        admin_mailer = double
+        mailer_double = double
+        expect(AdministratorNotifications::ChannelNotificationsMailer).to receive(:with).and_return(admin_mailer)
+        expect(admin_mailer).to receive(:email_disconnect).with(channel.inbox).and_return(mailer_double)
+        expect(mailer_double).to receive(:deliver_later)
+        channel.prompt_reauthorization!
+      end
+    end
+  end
+
+  it 'has a valid name' do
+    expect(channel.name).to eq('Email')
+  end
+
+  context 'when microsoft?' do
+    it 'returns false' do
+      expect(channel.microsoft?).to be(false)
+    end
+
+    it 'returns true' do
+      channel.provider = 'microsoft'
+      expect(channel.microsoft?).to be(true)
+    end
+  end
+
+  context 'when gmail provider' do
+    it 'auto save imap smtp channel' do
+      gmail_channel = create(:channel_email, provider: 'gmail')
+
+      expect(gmail_channel.imap_address).to eq('imap.gmail.com')
+      expect(gmail_channel.imap_enabled).to be_falsy
+    end
+  end
+end
