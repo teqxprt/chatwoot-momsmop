c867a15bbd5a6cb2fa205cc13ef22d10194b75ff
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-03-11 21:50:30 +0530

    Merge branch 'develop' into CW-3013

---
+++ app/builders/contact_inbox_with_contact_builder.rb
@@ -0,0 +1,105 @@
+# This Builder will create a contact and contact inbox with specified attributes.
+# If an existing identified contact exisits, it will be returned.
+# for contact inbox logic it uses the contact inbox builder
+
+class ContactInboxWithContactBuilder
+  include ContactHelper
+  pattr_initialize [:inbox!, :contact_attributes!, :source_id, :hmac_verified]
+
+  def perform
+    find_or_create_contact_and_contact_inbox
+  # in case of race conditions where contact is created by another thread
+  # we will try to find the contact and create a contact inbox
+  rescue ActiveRecord::RecordNotUnique
+    find_or_create_contact_and_contact_inbox
+  end
+
+  def find_or_create_contact_and_contact_inbox
+    @contact_inbox = inbox.contact_inboxes.find_by(source_id: source_id) if source_id.present?
+    return @contact_inbox if @contact_inbox
+
+    ActiveRecord::Base.transaction(requires_new: true) do
+      build_contact_with_contact_inbox
+      update_contact_avatar(@contact) unless @contact.avatar.attached?
+      @contact_inbox
+    end
+  end
+
+  private
+
+  def build_contact_with_contact_inbox
+    @contact = find_contact || create_contact
+    @contact_inbox = create_contact_inbox
+  end
+
+  def account
+    @account ||= inbox.account
+  end
+
+  def create_contact_inbox
+    ContactInboxBuilder.new(
+      contact: @contact,
+      inbox: @inbox,
+      source_id: @source_id,
+      hmac_verified: hmac_verified
+    ).perform
+  end
+
+  def update_contact_avatar(contact)
+    ::Avatar::AvatarFromUrlJob.perform_later(contact, contact_attributes[:avatar_url]) if contact_attributes[:avatar_url]
+  end
+
+  def create_contact
+    # TODO: Consider name as first_name and we will change the name to full_name in the future
+    name, middle_name, last_name = extract_name_parts
+    account.contacts.create!(
+      name: name,
+      middle_name: middle_name,
+      last_name: last_name,
+      phone_number: contact_attributes[:phone_number],
+      email: contact_attributes[:email],
+      identifier: contact_attributes[:identifier],
+      additional_attributes: contact_attributes[:additional_attributes],
+      custom_attributes: contact_attributes[:custom_attributes]
+    )
+  end
+
+  def extract_name_parts
+    # Return a generated name if name attribute is blank
+    return [::Haikunator.haikunate(1000), '', ''] if contact_attributes[:name].blank?
+
+    # Return the all name parts if middle_name or last_name is present
+    if contact_attributes[:middle_name].present? || contact_attributes[:last_name].present?
+      return [contact_attributes[:name], contact_attributes[:middle_name], contact_attributes[:last_name]]
+    end
+
+    # If name is present, split it into first and last name
+    name_parts = parse_name(contact_attributes[:name])
+    [name_parts[:first_name], name_parts[:middle_name], name_parts[:last_name]]
+  end
+
+  def find_contact
+    contact = find_contact_by_identifier(contact_attributes[:identifier])
+    contact ||= find_contact_by_email(contact_attributes[:email])
+    contact ||= find_contact_by_phone_number(contact_attributes[:phone_number])
+    contact
+  end
+
+  def find_contact_by_identifier(identifier)
+    return if identifier.blank?
+
+    account.contacts.find_by(identifier: identifier)
+  end
+
+  def find_contact_by_email(email)
+    return if email.blank?
+
+    account.contacts.from_email(email)
+  end
+
+  def find_contact_by_phone_number(phone_number)
+    return if phone_number.blank?
+
+    account.contacts.find_by(phone_number: phone_number)
+  end
+end
